1dd784b27a002da07f992179a1d4be3049565480
Valentin Clement <clementval@gmail.com>
2018-03-01 14:15:09 +0100

    Add OpenACC directives

--- elemental/std/program.f90
+++ elemental/std/program.f90
@@ -4,12 +4,17 @@ program elemental_test
   real, dimension(100) :: arg1, arg2, res
   integer :: i
 
+  !$acc data create(arg1, arg2, create)
+  !$acc parallel loop gang vector
   do i = 1, 100
     arg1(i) = i*i
     arg2(i) = i+i
   end do
-
+  
+  !$acc kernels
   res(:) = do_some_stuff(arg1(:), arg2(:))
+  !$acc end kernels
 
+  !$acc end data
   print*, sum(res(:))
 end program elemental_test
